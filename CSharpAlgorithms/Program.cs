6149285edf8d9ee1920fcc9604549756fb5bd591
Carl Kakisis <Messinias1@gmail.com>
2020-02-16 23:47:46 -0500

    alt solution

--- CSharpAlgorithms/Program.cs
+++ CSharpAlgorithms/Program.cs
@@ -30,3 +30,40 @@ class Solution
     }
 }
 
+//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//class Solution
+//{
+//    static void Main(String[] args)
+//    {
+//        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+//        var T = Convert.ToInt32(Console.ReadLine());
+//        for (var t = 0; t < T; t++)
+//        {
+//            int n, i, flag = 0;
+
+//            n = int.Parse(Console.ReadLine());
+//            if (n == 1)
+//            {
+//                Console.WriteLine("Not prime");
+//            }
+
+//            for (i = 2; i <= Math.Sqrt(n); i++)
+//            {
+//                if (n % i == 0)
+//                {
+//                    Console.WriteLine("Not prime");
+//                    flag = 1;
+//                    break;
+//                }
+//            }
+//            if (n >= 2 && flag == 0)
+//                Console.WriteLine("Prime");
+//        }
+
+//    }
+//}
+
+
+
